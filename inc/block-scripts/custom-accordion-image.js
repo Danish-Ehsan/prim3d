854e01659cf62390ad88fc0f7597cc8284f0ca5f
Danish Ehsan <danish.ehsan321@gmail.com>
2020-12-17 05:23:04 -0500

    Image Accordion Block - Removed console logs from script

--- inc/block-scripts/custom-accordion-image.js
+++ inc/block-scripts/custom-accordion-image.js
@@ -12,20 +12,13 @@
 		$(this).css( 'height', '0' );
 	});
 	
-	console.log(slidePanelHeightsArray);
-	console.log($slidePanel);
-	
 	//When panel is opened, animate the height to the full size of the panel
 	$accordionTopPanel.on( 'click', function() {
-		//console.log($(this).parent('.js--custom-accordion-image'));
-		console.log($(this).parent().index('.js--custom-accordion-image'));
 		if ( !$(this).parent().hasClass('open') ) {
-			console.log('test 1');
 			$(this).parent('.js--custom-accordion-image').removeClass('open closed').addClass('open');
 			$slidePanel.eq( ($(this).parent().index('.js--custom-accordion-image')) ).css( 'height', slidePanelHeightsArray[($(this).parent().index('.js--custom-accordion-image'))] + 'px' );
 		} else {
 			$(this).parent('.js--custom-accordion-image').removeClass('open closed').addClass('closed');
-			console.log('test 2');
 			$slidePanel.eq( ($(this).parent().index('.js--custom-accordion-image')) ).css( 'height', '0' );
 		}
 	});
