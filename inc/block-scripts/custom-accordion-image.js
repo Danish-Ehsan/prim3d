4aeb14628e5b41fb79d1ac096725c1cd91e25797
Danish Ehsan <danish.ehsan321@gmail.com>
2021-01-13 03:43:23 -0500

    Custom Accordion Image - Slide panel height recalculates if window is resized

--- inc/block-scripts/custom-accordion-image.js
+++ inc/block-scripts/custom-accordion-image.js
@@ -3,6 +3,7 @@
 	var $accordionTopPanel = $('.js--custom-accordion-image-top-panel');
 	var $slidePanel = $('.js--slide-panel');
 	var slidePanelHeightsArray = [];
+	var windowResizeTimer;
 	
 	//Set height to auto to grab the panel height and store it in an array 
 	//for animating height when panel opens
@@ -10,6 +11,23 @@
 		$(this).css( 'height', 'auto' );
 		slidePanelHeightsArray.push( $(this).height() );
 		$(this).css( 'height', '0' );
+	});
+	
+	//If window is resize recalute the height of all slide panels
+	$(window).on( 'resize', function() {
+		console.log('resize test');
+		
+		//Clear timeout so the function only runs if the window stops being resized
+		clearTimeout(windowResizeTimer);
+		windowResizeTimer = setTimeout(function() {
+			console.log('running timer function');
+			slidePanelHeightsArray = [];
+			$slidePanel.each(function() {
+				$(this).css( 'height', 'auto' );
+				slidePanelHeightsArray.push( $(this).height() );
+				$(this).css( 'height', '0' );
+			});
+		}, 200);
 	});
 	
 	//When panel is opened, animate the height to the full size of the panel
@@ -22,5 +40,5 @@
 			$slidePanel.eq( ($(this).parent().index('.js--custom-accordion-image')) ).css( 'height', '0' );
 		}
 	});
-   
+
 }( jQuery ));
